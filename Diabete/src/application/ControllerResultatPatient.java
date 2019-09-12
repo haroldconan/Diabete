89611388218199ca3a37b153c77a2fc17e802039
28har <28har@172.28.12.230>
2019-09-12 09:44:08 +0200

    ajout

---
+++ Diabete/src/application/ControllerResultatPatient.java
@@ -0,0 +1,47 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+
+public class ControllerResultatPatient {
+	
+	public void NouvelleEntree(ActionEvent event) {
+		System.out.println("Button Clicked!");
+	       JOptionPane jop1;
+	       
+	     //Boîte du message d'information
+	       Main.replaceSceneContent("designInfoPatient.fxml");
+	}
+	public void Statistiques(ActionEvent event) {
+	       System.out.println("Button Clicked!");
+	       JOptionPane jop1;
+	       
+	     //Boîte du message d'information
+	       Main.replaceSceneContent("designStat.fxml");
+	      
+	   }
+	
+	public void Accueil(ActionEvent event) {
+	       System.out.println("Button Clicked!");
+	       JOptionPane jop1;
+	       
+	     //Boîte du message d'information
+	       Main.replaceSceneContent("design.fxml");
+	      
+	   }
+	public void Deconnexion(ActionEvent event) {
+	       System.out.println("Button Clicked!");
+	       JOptionPane jop1;
+	       
+	     //Boîte du message d'information
+	     Main.replaceSceneContent("designLog.fxml");
+	   }
+	public void Valider(ActionEvent event) {
+	       System.out.println("Button Clicked!");
+	       JOptionPane jop1;
+	       
+	     //Boîte du message d'information
+	     Main.replaceSceneContent("design.fxml");
+	   }
+}

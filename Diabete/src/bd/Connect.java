361b5362500e21bb91d4bad529c10fbdaefedaad
Antoine <antoine.pascaud@etu.unilim.fr>
2019-10-03 11:59:30 +0200

    Création de la base à la première connexion

--- Diabete/src/bd/Connect.java
+++ Diabete/src/bd/Connect.java
@@ -24,18 +24,19 @@ public class Connect {
 			String tableIndividu = "CREATE TABLE IF NOT EXISTS individu (\n" + "    id integer PRIMARY KEY,\n"
 					+ "    nom text NOT NULL,\n" + "    prenom text NOT NULL,\n" + "    dateNaissance text NOT NULL,\n"
 					+ "    sexe int NOT NULL,\n" + "    numSecu int NOT NULL \n" + ");";
-			
+
 			String tableDonnees = "CREATE TABLE IF NOT EXISTS donnees (\n" + "    id integer PRIMARY KEY,\n"
-					+ "idIndividu int NOT NULL"
-					+ "FOREIGN KEY(idIndividu) REFERENCES individu(id),\n"
-					+ "    nom text NOT NULL,\n" + "    prenom text NOT NULL,\n" + "    dateNaissance text NOT NULL,\n"
-					+ "    sexe int NOT NULL,\n" + "    numSecu int NOT NULL \n" + ");";
-			
+					+ "idIndividu int NOT NULL,\n" + "taille int NOT NULL,\n" + "poids int NOT NULL, \n"
+					+ "age int NOT NULL,\n" + "tourDeTaille int NOT NULL,\n" + "actPhy int NOT NULL,\n"
+					+ "atcdAntiHTA int NOT NULL,\n" + "atcdFamille int NOT NULL,\n" + "atcdGlycemie int NOT NULL,\n"
+					+ "legumeVert int NOT NULL, \n" + "resultat int NOT NULL,\n" + "date text NOT NULL,\n"
+					+ "FOREIGN KEY(idIndividu) REFERENCES individu(id) );";
+
 			System.out.println("Connection to SQLite has been established.");
 			Statement stmt = conn.createStatement();
 			stmt.execute(tableIndividu);
 			stmt.execute(tableDonnees);
-			//stmt.execute(query);
+			// stmt.execute(query);
 			System.out.println("BD");
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
